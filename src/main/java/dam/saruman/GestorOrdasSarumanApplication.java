5141072c881ce2cb6df07ca2ef234be01dd79f35
polhm25 <polhm25@gmail.com>
2025-12-04 13:52:00 +0100

    Añade clase principal y configuración de base de datos PostgreSQL

---
+++ src/main/java/dam/saruman/GestorOrdasSarumanApplication.java
@@ -0,0 +1,12 @@
+package dam.saruman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorOrdasSarumanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorOrdasSarumanApplication.class, args);
+    }
+}
